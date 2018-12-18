5b2ce05be2ef3fb6d0c320b34ea9317db8922075
Linhuijie <huijie.lin.b@gmail.com>
2018-12-18 14:52:08 +0800

    add project

---
+++ TravisTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravisTest
+//
+//  Created by linhuijie on 2018/12/18.
+//  Copyright © 2018年 linhuijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TravisTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TravisTest
+//
+//  Created by linhuijie on 2018/12/18.
+//  Copyright © 2018年 linhuijie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
