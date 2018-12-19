8f3060ea0f39c18e85debd31450192cca61d762a
LinHuijie <huijie.lin.b@gmail.com>
2018-12-19 12:10:59 +0800

    111

--- aaaaTests/aaaaTests.m
+++ aaaaTests/aaaaTests.m
@@ -16,7 +16,7 @@
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    NSLog(@"")
+    NSLog(@"--");
 }
 
 - (void)tearDown {
